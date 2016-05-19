325854c24d6dd9f9c5465a2857255a2d89c72ed2
wangye <wangshenyege@gmail.com>
2016-05-19 18:51:11 +0800

    first create project

--- polls/views.py
+++ polls/views.py
@@ -21,23 +21,24 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    p = get_object_or_404(Question, pk = question_id)
+    p = get_object_or_404(Question, pk=question_id)
+
     try:
-        selected_choice = p.choice_set.get(pk = request.POST['choice'])
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except(KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
-            'question':p,
+            'question': p,
             'error_message': "You didn't select a choice."
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id)))
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
 
 def detail(request, question_id):
@@ -45,5 +46,5 @@ def detail(request, question_id):
     #     question = Question.objects.get(pk = question_id)
     # except Question.DoesNotExist:
     #     raise Http404('Question does not Exist!')
-    question = get_object_or_404(Question, pk = question_id)
-    return render(request, 'polls/detail', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
